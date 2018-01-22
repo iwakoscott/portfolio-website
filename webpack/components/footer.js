4358fd54c251420eece178e19e0f358a33c694d1
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-01-21 20:45:26 -0800

    add footer

---
+++ webpack/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer(props){
+  return (
+    <div className="container-fluid card-board">
+      <div className="row">
+        <div className="col-sm-3">
+        </div>
+        <div className="col-sm-6">
+        </div>
+        <div className="col-sm-3">
+          <span>{"COPYRIGHT "} &copy; {" 2018 Satoshi Scott Iwako"} </span>
+        </div>
+      </div>
+    </div>
+  );
+}
